779102b1f12e7a7c116f75ff10569ed4a6b86de0
InSeon6798 <dlstjs316@naver.com>
2023-08-14 14:56:18 +0900

    로그인, 이미지 연결, 병합

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 
 app = Flask(__name__)
@@ -34,32 +34,52 @@ def main():
 #로그인
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # 이 예시에서는 간단하게 사번과 비밀번호가 일치하면 로그인 성공으로 가정합니다.
+        query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+        cursor.execute(query)
+        user_data = cursor.fetchone()
+
+        if user_data:
+            session['username'] = username
+            return redirect(url_for('page2')) #데이터 검증 성공하면 main으로 들어가진다
+
     return render_template('login.html')
 
-@app.route('/page2.html')
+#로그아웃
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/main.html')
 def page2():
     return render_template('main.html')
 
 #인적사항 데베
-@app.route('/1')
+@app.route('/human')
 def index():
     page = request.args.get('page', default=1, type=int)
     per_page = 10
 
     current_data, columns, total_pages = get_page_data("em_json", page, per_page)
 
-    return render_template('dlswjr.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
+    return render_template('human.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
 
 
 #채용사항 데베
-@app.route('/2')
+@app.route('/employment')
 def test():
     page = request.args.get('page', default=1, type=int)
     per_page = 10
 
     current_data, columns, total_pages = get_page_data("today", page, per_page)
 
-    return render_template('test.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
+    return render_template('employment.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
 
 
 if __name__ == '__main__':
