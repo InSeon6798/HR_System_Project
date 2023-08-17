bca23fcf8140432202fb88dd7170a65c63a969da
vittroi384 <eunsol924@naver.com>
2023-08-17 12:56:14 +0900

    검색창,정령 기능추가

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 
-app = Flask(__name__)
+app = Flask(__name__)   
 
 db = mysql.connector.connect(
     host="localhost",
@@ -12,24 +12,22 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-# 공유하는 함수로 데이터를 가져오는 로직을 구현
-def get_page_data(table_name, page, per_page):
-    cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
-    total_rows = cursor.fetchone()[0]
-    total_pages = (total_rows + per_page - 1) // per_page
 
-    cursor.execute(f"SHOW COLUMNS FROM {table_name}")  # 테이블의 컬럼 정보 조회
+# 공유하는 함수로 데이터를 가져오는 로직을 구현
+def get_all_data(table_name):
+    cursor.execute(f"SHOW COLUMNS FROM {table_name}")  # Fetch columns
     columns = [column[0] for column in cursor.fetchall()]
 
-    start_idx = (page - 1) * per_page
-    cursor.execute(f"SELECT * FROM {table_name} LIMIT {start_idx},{per_page}")
-    current_data = cursor.fetchall()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    all_data = cursor.fetchall()
 
-    return current_data, columns, total_pages
+    return all_data, columns
+
 
 @app.route('/')
 def main():
     return render_template('login.html')
+
 
 #로그인
 @app.route('/login', methods=['GET', 'POST'])
@@ -45,6 +43,7 @@ def login():
 
         if user_data:
             session['username'] = username
+            print("로그인성공")
             return redirect(url_for('page2')) #데이터 검증 성공하면 main으로 들어가진다
 
     return render_template('login.html')
@@ -53,35 +52,24 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    print("로그아웃 성공")
+    print("로그아웃 성공")  
     return redirect(url_for('login'))
 
-
-@app.route('/main.html')
+@app.route('/main')
 def page2():
     return render_template('main.html')
 
 #인적사항 데베
 @app.route('/human')
 def index():
-    page = request.args.get('page', default=1, type=int)
-    per_page = 10
-
-    current_data, columns, total_pages = get_page_data("em_json", page, per_page)
-
-    return render_template('human.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
-
+    current_data, columns = get_all_data("em_json")
+    return render_template('human.html', data=current_data, columns=columns)
 
 #채용사항 데베
 @app.route('/employment')
 def test():
-    page = request.args.get('page', default=1, type=int)
-    per_page = 10
-
-    current_data, columns, total_pages = get_page_data("today", page, per_page)
-
-    return render_template('employment.html', data=current_data, columns=columns, page=page, total_pages=total_pages)
-
+    current_data, columns = get_all_data("today")
+    return render_template('employment.html', data=current_data, columns=columns)
 
 if __name__ == '__main__':
     app.secret_key = 'your_secret_key'
